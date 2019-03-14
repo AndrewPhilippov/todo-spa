c64f6a3643089d9b5002c6a8bc9d347d61daca62
Andrew <a@fandb.pro>
2019-03-14 08:44:07 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+    app = express();
+
+app.get('/', function (req, res) {
+    res.render("index");
+});
+
+
+var port = process.env.PORT || '8080';
+var ip = process.env.IP || '127.0.0.1';
+
+
+app.listen(port, ip, function () {
+    var date = new Date();
+    console.log('Server is listening on ' + port + ' at ' + date);
+});
