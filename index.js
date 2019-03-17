226be58df1b9d8a4b8a0aae45542920943f77f95
Andrew <a@fandb.pro>
2019-03-17 19:34:43 +0300

    views, public css and js added

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 
 app.get('/', function (req, res) {
-    res.send("Hello from root route");
+    res.sendFile('index.html');
 });
 app.use('/api/todos', todoRoutes);
 
